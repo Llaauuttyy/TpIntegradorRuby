219e3c97378be4923d4debca1f07b048fb56a574
Llaauuttyy <ldabbraccio@fi.uba.ar>
2022-05-20 23:56:29 -0300

    Actualizo y agrego llamada a ConnectionHandler.

--- main.rb
+++ main.rb
@@ -2,43 +2,12 @@ require "mechanize"
 require "nokogiri"
 
 require 'pg'
-require 'active_record'
 
-require_relative 'credentials'
+require_relative 'connection.rb'
+require_relative 'utils.rb'
 
-# mechanize = Mechanize.new
+# Creamos conexion apuntando a la base 'supermarket'
+connectionHandler = ConnectionHandler.new(DB_CONFIG)
 
-# page = mechanize.get('https://es.wikipedia.org/wiki/Wikipedia:Portada')
-
-# puts page.xpath("//a[contains(@title,'Wikipedia')]")[0].text
-
-# definimos el hash de configuracion de la base
-db_config = {
-	host: 'localhost',
-	adapter: 'postgresql',
-	encoding: 'utf-8',
-	database: 'supermarket',
-	schema_search_path: 'public'
-}
-
-db_config = db_config.merge({username: 'postgres', password: PASSWORD})
-
-# establecemos conexion con la base de datos
-ActiveRecord::Base.establish_connection(db_config)
-
-
-# creamos una tabla
-class CreateTable < ActiveRecord::Migration[5.0]
-    def change
-        create_table    :toBuyList
-
-        t.string    :id
-
-        t.text      :product_name
-
-        t.text      :price
-    end
-end
-
-# creamos una instancia y llamamos a change para crear la tabla
-CreateTable.new.change
+# Ejecutamos query dentro de esa base
+connectionHandler.executeQuery('')
